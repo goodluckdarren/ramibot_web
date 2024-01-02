2be2c46ac4e6d693fa178de7070fb08b83284564
goodluckdarren <darrenderige1111@gmail.com>
2024-01-02 18:52:40 +0800

    improved ui
    better table and search and sort header added with no back end yet

--- home/new_user_files.php
+++ home/new_user_files.php
@@ -1,12 +1,16 @@
+<?php require_once('../home/database_connect.php');?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <title>ADMIBOT</title>
+    <title>ADMIBOT</title> 
     <link rel="stylesheet" href="../home/homepage.css">
     <link rel="stylesheet" href="../home/interactions.css">
+    <link rel="stylesheet" href="../home/info_user_files_table.css">
     <link href='https://fonts.googleapis.com/css?family=Montserrat' rel='stylesheet'>
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v6.3.0/css/all.css">
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
     <script src="../home/redirect.js"></script>
 </head>
 <body class="home-page">
@@ -15,7 +19,7 @@
             <div class="home-btn-group">
                 <div class="main-btn btn new-user-files" onclick="redirectToUserFiles()">
                     <div class="main-btn-box"></div>
-                    <p>User Files</p>  
+                    <p>New User Files</p>  
                 </div>
                 <div class="btn faculty-schedule" onclick="redirectToSchedule()">
                     <p>Faculty Schedule</p>
@@ -33,14 +37,52 @@
         </div>
         <div class="content">
             <div class="top-header">
+                <div class="page-title">
+                    <p class="page-name">NEW USER FILES</p>
+                </div>
                 <div class="user-profile">
                     <p class="user-name">BARACK OBAMA</p>
-                    <img class ="profile-picture" src="../images_home/obama sample.jpg" alt="Image of Admin" width="70px" height="70px">
+                    <img class ="profile-picture" src="../images/obama sample.jpg" alt="Image of Admin" width="70px" height="70px">
                 </div>
             </div>
             <div class="main-menu">
                 <div class="main-panel">
-                    Lorem ipsum dorem
+                    <div class = "sort-search-box">
+                        <div class = "search-box">
+                                <p class="sort-search-title">Search:</p>
+                                <input class="search-input" type="text" id="search-input" name="search" placeholder="Search..">
+                        </div>
+                        <div class = "sort-box">
+                            <p class="sort-search-title">Sort by:</p>
+                            <select class="sort-select" name="sort" id="sort-select">
+                                <option value="id_number">ID Number</option>
+                                <option value="profession">Profession</option>
+                                <option value="last_name">Last Name</option>
+                                <option value="given_name">Given Name</option>
+                                <option value="middle_initial">Middle Initial</option>
+                                <option value="nickname">Nickname</option>
+                            </select>
+                        </div>
+                    </div>
+                    <div class = "table-container">
+                    <table class="user-files-table">
+                        <thead>
+                            <tr>
+                                <th>ID Number</th>
+                                <th>Profession</th>
+                                <th>Last Name</th>
+                                <th>Given Name</th>
+                                <th>Middle Initial</th>
+                                <th>Nickname</th>
+                            </tr>
+                        </thead>
+                        <tbody id="user-files-table-content">
+                        <?php include '../home/user_files_table.php'?>
+                        </tbody>
+                    </table>
+                    </div>
+                    <ul id="pagination" class="pagination">
+                    </ul>
                 </div>
             </div>
         </div>
---
+++ home/user_files_table.php
@@ -0,0 +1,93 @@
+<?php
+    require_once('../home/database_connect.php');
+
+    $rows_per_page = 10;
+
+    $page = isset($_GET['page']) ? (int)$_GET['page'] : 1;
+
+    $offset = ($page - 1) * $rows_per_page;
+
+    $count_query = "SELECT COUNT(*) as count FROM ramibot_faces";
+    $count_result = mysqli_query($con, $count_query);
+    $count_row = mysqli_fetch_assoc($count_result);
+    $total_rows = $count_row['count'];
+
+    $total_pages = ceil($total_rows / $rows_per_page);
+
+    $sql = "SELECT rf.* 
+        FROM ramibot_faces AS rf
+        -- ORDER BY rf.ID_Number ASC
+        LIMIT $offset, $rows_per_page";
+    $result_table = mysqli_query($con, $sql);
+
+    while ($row = mysqli_fetch_assoc($result_table)) {
+        echo "<tr>";
+        echo "<td>" . $row['ID_Number'] . "</td>";
+        echo "<td>" . $row['Profession'] . "</td>";
+        echo "<td>" . $row['Last_Name'] . "</td>";
+        echo "<td>" . $row['Given_Name'] . "</td>";
+        echo "<td>" . $row['MI'] . "</td>";
+        echo "<td>" . $row['nickname'] . "</td>";
+        echo "</tr>";
+    }
+?>
+
+<script>
+    // Declare global variables
+    var currentPage = <?php echo $page; ?>;
+    var totalPages = <?php echo $total_pages; ?>;
+
+    $(document).ready(function () {
+        updatePagination();
+    });
+
+    function updatePagination() {
+        var paginationHtml = '';
+        var maxButtons = 5;
+
+        var startPage = Math.max(1, currentPage - Math.floor(maxButtons / 2));
+        var endPage = Math.min(totalPages, startPage + maxButtons - 1);
+
+        if (endPage - startPage < maxButtons - 1) {
+            startPage = Math.max(1, endPage - maxButtons + 1);
+        }
+
+        // Previous button
+        paginationHtml += '<li class="page-item ' + (currentPage === 1 ? 'disabled' : '') + '"><a class="page-link" href="#" onclick="loadPage(' + (currentPage - 1) + ')">&laquo;</a></li>';
+
+        for (var i = startPage; i <= endPage; i++) {
+            paginationHtml += '<li class="page-item ' + (i === currentPage ? 'active' : '') + '"><a class="page-link" href="#" onclick="loadPage(' + i + ')">' + i + '</a></li>';
+        }
+
+        // Next button
+        paginationHtml += '<li class="page-item ' + (currentPage === totalPages ? 'disabled' : '') + '"><a class="page-link" href="#" onclick="loadPage(' + (currentPage + 1) + ')">&raquo;</a></li>';
+
+        $('#pagination').html(paginationHtml);
+    }
+
+    function loadPage(page) {
+        if (page < 1 || page > totalPages || page === currentPage) {
+            return;
+        }
+
+        currentPage = page;
+        updatePagination();
+        loadTableContent();
+    }
+
+    function loadTableContent() {
+        $.ajax({
+            url: '../home/user_files_table.php',
+            type: 'GET',
+            data: { page: currentPage },
+            success: function (data) {
+                $('#user-files-table-content').fadeOut('fast', function () {
+                    $(this).html(data).fadeIn('fast');
+                });
+            },
+            error: function () {
+                alert('Error loading table content.');
+            }
+        });
+    }
+</script>
