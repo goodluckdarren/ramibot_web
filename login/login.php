7a55093441f00b9bd7c46cb95d9c5ebcec02a9ce
goodluckdarren <darrenderige1111@gmail.com>
2024-03-13 21:59:08 +0800

    Working Login
    Needs update on design

--- login/login.php
+++ login/login.php
@@ -3,32 +3,31 @@
     <head>
         <meta charset="UTF-8">
         <title>ADMIBOT</title>
-        <link rel="stylesheet" href="../login.css">
-
-
+        <link rel="stylesheet" href="login.css">
     </head>
     <body class="login-page">
-        <div class="blur"></div>
         <div class="logo">
             <img id="nu-apc-logo.png" src="../login/nu-apc-logo.png" width="300px" height="150px">
         </div>
         <div class="login-box" >
             <div class="justified-center">
-                Admin Login
+                ADMIN LOGIN
             </div>
-            <form class="sign-in-form" name="signin-form" method="POST" autocomplete="off">
+            <form action="../login/login_backend/login_authenticate.php" name="signin-form" method="POST" autocomplete="off">
                 <input class="email-input" id="email_field" name="email" type="email" placeholder="Email Address" autocomplete="email" autofocus required>
             <i class="fas fa-envelope"></i>
             <input id="password_field" name="password" type="password" placeholder="Password" autocomplete="password" required>
             <i class="fas fa-eye" onclick="show_password()"></i>
-         </form>
-         <div class="forgot-password">
+            <div class="forgot-password">
                 <a href="" id="password-reset"> forgot password? </a>
          </div>
             <button type="submit" name="login" class="signin-button button">
                <span class="fas fa-sign-in-alt"></span>
                Log In
             </button>
+         </form>
+
+
         </div>
     </body>
 </html>
